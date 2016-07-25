5e41e14be4ef5aeff0a4d194cd78b4065aab1024
DrDombor <rs1989@rambler.ru>
2016-07-25 20:46:22 +0300

    Update задание2.sql

--- задание2.sql
+++ задание2.sql
@@ -1,5 +1,5 @@
 --Исхожу из условия что у нас есть товары именно "молоко" и "сметана". Таблица с клиентами - Clients, таблица с покупками Purchases.
  
  select CustomerId, RegistrationDateTime from Clients 
- where CustomerId not in (select CustomerId from Purchases where ProductName = 'ñìåòàíà') 
- and CustomerId in (select CustomerId from Purchases where ProductName = 'ìîëîêî') 
+ where CustomerId not in (select CustomerId from Purchases where ProductName = 'сметана') 
+ and CustomerId in (select CustomerId from Purchases where ProductName = 'молоко') 
